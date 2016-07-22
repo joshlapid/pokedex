46cf344e2a3f8ac64fab7aea298dd9569cdd9077
Joshua Lapid <lapidjoshua@yahoo.com>
2016-07-22 10:46:14 -0700

    Added more pokemon to the database, added tables and jumbotron

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,8 @@
 class SearchController < ApplicationController
   def search
-    # @search_results = Pokemon.fuzzy_search({number: params[:search_input], name: params[:search_input], element: params[:search_input]})
-    # @search_results = Pokemon.fuzzy_search(name: params[:search_input])
-    @search_results = Pokemon.fuzzy_search(name: params[:search_input])
-    @search_results2 = Pokemon.fuzzy_search(element: params[:search_input])
-    @search_results3 = Pokemon.fuzzy_search(number: params[:search_input])
+    @search_results_name = Pokemon.fuzzy_search(name: params[:search_input])
+    @search_results_element = Pokemon.fuzzy_search(element: params[:search_input])
+    @search_results_description = Pokemon.fuzzy_search(description: params[:search_input])
     render 'search/search.html.erb'
   end
 
