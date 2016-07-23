54000d12681ecb2ef37ecbfd521ae1d227fca5b0
Joshua Lapid <lapidjoshua@yahoo.com>
2016-07-22 18:31:51 -0700

    fixed everything i broke

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,12 +4,18 @@ class SearchController < ApplicationController
     @search_results_name = Pokemon.fuzzy_search(name: params[:search_input])
     # Searches for pokemon by element
     @search_results_element = Pokemon.fuzzy_search(element: params[:search_input])
-    # Searches for pokemon by description
-    @search_results_description = Pokemon.fuzzy_search(description: params[:search_input])
-    # Searches for pokemon by first letter
-    @search_results_first_letter = Pokemon.where('substr(name, 1, 1) = ? OR substr(name, 1, 1) = ?', params[:search_input].upcase, params[:search_input].downcase)
-    # Searches for pokemon by first two letters
-    @search_results_two_letter = Pokemon.where('substr(name, 1, 2) = ? OR substr(name, 1, 2) = ?', params[:search_input], params[:search_input].capitalize)
+
+    if !params[:search_input].nil?
+      # Searches for pokemon by first letter
+      @search_results_first_letter = Pokemon.where('substr(name, 1, 1) = ? OR substr(name, 1, 1) = ?', params[:search_input].upcase, params[:search_input].downcase)
+    end
+      # Searches for pokemon by first two letters
+
+    if !params[:search_input].nil?
+      @search_results_two_letter = Pokemon.where('substr(name, 1, 2) = ? OR substr(name, 1, 2) = ?', params[:search_input], params[:search_input].capitalize)
+    end
+
+    render 'search/search.html.erb'
   end
 
 end
