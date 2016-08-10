c19b7d04cb0b91f857e1396e9084144149d18486
Joshua Lapid <lapidjoshua@yahoo.com>
2016-08-10 14:23:01 -0700

    updates

--- lib/poke_scrape.rb
+++ lib/poke_scrape.rb
@@ -14,7 +14,7 @@ class PokemonScraper
       html_pokemon = Nokogiri::HTML(page.body)
       number = html_pokemon.at_css('#pokemonID').text
       type1 = html_pokemon.xpath('//div[@class="dtm-type"]/ul/li/a')[0].text
-      if html_pokemon.xpath('//div[@class="dtm-ty`pe"]/ul/li/a')[1] != nil
+      if html_pokemon.xpath('//div[@class="dtm-type"]/ul/li/a')[1] != nil
         type2 = html_pokemon.xpath('//div[@class="dtm-type"]/ul/li/a')[1].text
       end
       description = html_pokemon.at_xpath('//div/p')
