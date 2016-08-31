69310eefb07f20b5375f769b61fc67414891a376
Joshua Lapid <lapidjoshua@yahoo.com>
2016-08-31 12:07:46 -0700

    updated search engine

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,27 +2,30 @@ class SearchController < ApplicationController
   def search
     @pokemon_all = Pokemon.all
     # Searches for pokemon by name
-    @search_results_name = Pokemon.fuzzy_search(name: params[:search_input])
     # Searches for pokemon by element
     @search_results_element = Pokemon.fuzzy_search(element: params[:search_input])
 
     if !params[:search_input].nil?
-      # Searches for pokemon by first letter
-      @search_results_first_letter = Pokemon.where('substr(name, 1, 1) = ? OR substr(name, 1, 1) = ?', params[:search_input].upcase, params[:search_input].downcase)
-    end
-      # Searches for pokemon by first two letters
+      if params[:search_input].length == 1
+        # Searches for pokemon by first letter
+        @search_results = Pokemon.where('substr(name, 1, 1) = ? OR substr(name, 1, 1) = ?', params[:search_input].upcase, params[:search_input].downcase)
 
-    if !params[:search_input].nil?
-      @search_results_two_letter = Pokemon.where('substr(name, 1, 2) = ? OR substr(name, 1, 2) = ?', params[:search_input], params[:search_input].capitalize)
+      # Searches for pokemon by first 2 letters
+      elsif params[:search_input].length == 2
+        @search_results = Pokemon.where('substr(name, 1, 2) = ? OR substr(name, 1, 2) = ?', params[:search_input], params[:search_input].capitalize)
+
+      # Searches for pokemon by first 3 letters
+      elsif params[:search_input].length == 3
+        @search_results = Pokemon.where('substr(name, 1, 3) = ? OR substr(name, 1, 3) = ?', params[:search_input], params[:search_input].capitalize)
+      else
+        @search_results = Pokemon.fuzzy_search(name: params[:search_input])
+      end
     end
 
-    if !params[:search_input].nil?
-      @search_results_three_letter = Pokemon.where('substr(name, 1, 3) = ? OR substr(name, 1, 3) = ?', params[:search_input], params[:search_input].capitalize)
-    end
 
-    if params[:search_input].nil?
-      @pokemon_all = Pokemon.all
-    end
+      if params[:search_input].nil?
+        @pokemon_all = Pokemon.all
+      end
 
     render 'search/search.html.erb'
   end
